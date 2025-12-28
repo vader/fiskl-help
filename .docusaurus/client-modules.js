0402bd2d316c6ed7c350e0d2d858af2457082431
Shawn Vader <shawn@fiskl.com>
2025-12-28 16:21:59 +0000

    Updated styles

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,7 +1,7 @@
 export default [
-  require("/Users/vader/Development/fiskl/fiskl-help/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/vader/Development/fiskl/fiskl-help/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/vader/Development/fiskl/fiskl-help/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_react-dom@19.2.3_react@19.2.3__react@19.2.3_typescript@5.6.3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/vader/Development/fiskl/fiskl-help/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_react-dom@19.2.3_react@19.2.3__react@19.2.3_typescript@5.6.3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/vader/Development/fiskl/fiskl-help/src/css/custom.css"),
+  require("/Users/shawnvader/Development/fiskl/fiskl-help/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/shawnvader/Development/fiskl/fiskl-help/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/shawnvader/Development/fiskl/fiskl-help/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/shawnvader/Development/fiskl/fiskl-help/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/shawnvader/Development/fiskl/fiskl-help/src/css/custom.css"),
 ];
